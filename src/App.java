d585dbbfd97b402e32b067877ab536d03ff8496a
Tim <tim.englund@elev.ga.ntig.se>
2023-09-04 09:41:55 +0200

    Skapat kalkylering fil

---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("BOM BOM KNAS BRE SKOTT PÅ HUSFASADEN");
+        int tal1 = 23;
+        int tal2 = 45;
+        int summa
+        int produkt
+    
+    }
+}
