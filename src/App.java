12e4d0e5f0c3a7229ea9618ce589f120211c4dfd
Tim <tim.englund@elev.ga.ntig.se>
2023-09-06 10:21:03 +0200

    Lade till divisionsberäkningar

--- src/App.java
+++ src/App.java
@@ -1,9 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int tal1 = 23;
+        double tal1 = 23.0;
         int tal2 = 45;
-        int summa;  
+        double summa;  
         int produkt;
 
         summa = tal1+tal2;
@@ -20,6 +20,16 @@ public class App {
         produkt = tal2*tal3*tal4;
         System.out.println(tal2+"*"+tal3+"*"+tal4+"="+produkt);
         System.out.println("det blir fel");
+
+        double kvot;
+        kvot = tal1/tal2;
+        System.out.println(tal1+"/"+tal2+"="+kvot);
+
+        double tal5 = 10.0;
+        double tal6 = 3.0;
+
+        kvot=tal6/tal3;
+        System.out.println(tal6+"/"+tal3+"="+kvot);
     
     }
 }
