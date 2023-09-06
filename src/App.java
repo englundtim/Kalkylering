05ebb37164e0682ff39d1dfd673a49b75a985649
Tim <tim.englund@elev.ga.ntig.se>
2023-09-06 10:15:04 +0200

    Lade till felaktig multiplikationberäkning

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
         int tal1 = 23;
         int tal2 = 45;
         int summa;  
@@ -11,6 +11,15 @@ public class App {
 
         summa = tal1-tal2;
         System.out.println(tal1+" - "+tal2+" = "+summa);
+
+        int tal3 = 234232;
+        int tal4 = 3424;
+
+        produkt = tal3*tal4;
+        System.out.println(tal3+"*"+tal4+"="+produkt);
+        produkt = tal2*tal3*tal4;
+        System.out.println(tal2+"*"+tal3+"*"+tal4+"="+produkt);
+        System.out.println("det blir fel");
     
     }
 }
