a8088096a10e5bb31e2829b8a5ad7a83b91cebbd
Tim <tim.englund@elev.ga.ntig.se>
2023-09-06 10:02:54 +0200

    Implementerat beräkning för tal1+-tal2

--- src/App.java
+++ src/App.java
@@ -1,10 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("BOM BOM KNAS BRE SKOTT PÅ HUSFASADEN");
+        
         int tal1 = 23;
         int tal2 = 45;
-        int summa
-        int produkt
+        int summa;  
+        int produkt;
+
+        summa = tal1+tal2;
+        System.out.println(tal1+" + "+tal2+" = "+summa);
+
+        summa = tal1-tal2;
+        System.out.println(tal1+" - "+tal2+" = "+summa);
     
     }
 }
